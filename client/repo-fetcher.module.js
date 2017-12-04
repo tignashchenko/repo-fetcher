5c36eaa2993ed237bae20eb5f35b1d2b88a32299
Taras Ignashchenko <tignashchenko@gmail.com>
2017-12-04 15:04:51 +0200

    implement sorting function

--- client/repo-fetcher.module.js
+++ client/repo-fetcher.module.js
@@ -11,7 +11,18 @@ angular.module('repoFetcher', [])
       
       $http.get(`https://api.github.com/search/repositories?q=${$scope.searchTerm}`)
         .then(function(results) {
-          $scope.searchResults = results.data.items;
+          $scope.searchResults = results.data.items.sort(function(a, b) {
+            var name1 = a.name.toLowerCase();
+            var name2 = b.name.toLowerCase();
+
+            if (name1 < name2) {
+              return -1;
+            } else if (name1 > name2) {
+              return 1;
+            } else {
+              return 0;
+            }
+          });
           console.log($scope.searchResults);
         }, function(error) {
           console.log(error);
